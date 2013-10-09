47eb3df3bcb7cb2916db002aa7801b2853bf28e3
elizabethcheng <elizabeth.y.cheng@gmail.com>
2013-10-09 11:33:36 -0700

    Initial Commit
    Former-commit-id: 8e335926c6637722b4cd430ac6f8d6f7ec9dc4e8

---
+++ UI/testInstaller/testInstaller/testInstallerPane.h
@@ -0,0 +1,13 @@
+//
+//  testInstallerPane.h
+//  testInstaller
+//
+//  Created by echeng on 10/9/13.
+//  Copyright (c) 2013 echeng. All rights reserved.
+//
+
+#import <InstallerPlugins/InstallerPlugins.h>
+
+@interface testInstallerPane : InstallerPane
+
+@end
