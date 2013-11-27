08e47af2e789f671f53e577d97a45dbb15fc406a
elizabethcheng <elizabeth.y.cheng@gmail.com>
2013-11-26 17:32:15 -0800

    Modified Database.py, pre-install.sh, data.db
    Former-commit-id: 17775740e85d12cce5a70aabd89b14f2ca8635cb

--- UI/linux/pre-install.sh
+++ UI/linux/pre-install.sh
@@ -1,5 +1,13 @@
-sudo apt-get install python python-tk idle python-pmw python-imaging
-sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose python-pip python-setuptools sqlite3
+#!/bin/bash
+
+# Install python modules (tkinter, numpy, scipy, matplotlib, pandas, sqlite3, pip, setuptools
+sudo apt-get install python python-tk idle python-pmw python-imaging python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose python-pip python-setuptools sqlite3 python-imaging-tk
+
+# Install patsy
 sudo pip install --upgrade patsy
+
+# Install pandas version 0.7.1
 sudo easy_install pandas==0.7.1
+
+# Install statsmodels
 sudo easy_install -U statsmodels
